ee966960479e96d243704b4aadbc7de9091530e1
Sean Melody <seandmelody@gmail.com>
2021-05-28 16:55:52 -0700

    virtual environment created and working

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
-from app import routes
 from flask import Flask
 
 app = Flask(__name__)
 
 
 # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+from app import routes
