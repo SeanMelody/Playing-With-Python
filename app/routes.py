fe6268a341d8eebaa1b6df1bc580a407f079a0d7
Sean Melody <seandmelody@gmail.com>
2021-05-28 17:54:01 -0700

    python picking a password and displaying

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,28 @@
 from flask import render_template
 from app import app
-
+import random
+import string
 
 @app.route('/')
 @app.route('/index')
 # def index():
 #     return "Hello, World!"
 def index():
+
+        characters = string.ascii_letters + string.punctuation + string.digits
+
+
+        password = "".join(random.sample(characters, 15))
+
+
+
+        print("hello world")
+
         user = {'username': 'Arya'}
-        return render_template('index.html', title='Home', user=user)
+
+
+
+
+
+        
+        return render_template('index.html', title='Home', user=user, password=password)
