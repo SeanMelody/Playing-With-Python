b11119898fb003bd808d20c3405773ce351f875a
Sean Melody <seandmelody@gmail.com>
2021-05-28 17:09:02 -0700

    python dotenv added

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
+# def index():
+#     return "Hello, World!"
 def index():
-    return "Hello, World!"
+        user = {'username': 'Arya'}
+        return render_template('index.html', title='Home', user=user)
